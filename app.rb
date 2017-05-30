c76920b501a140372c4ef90ffe81530055a6cb1a
Steve Occhipinti <dev@stevenocchipinti.com>
2017-05-30 17:01:15 +1000

    Exercise #1 solution

--- app.rb
+++ app.rb
@@ -55,5 +55,5 @@ file_contents = File.read("guests.json")
 data = JSON.parse(file_contents)
 
 data["guests"].each do |guest|
-  puts guest
+  puts "#{guest["firstName"]} #{guest["secondName"]} was born in #{guest["birthYear"]}"
 end
